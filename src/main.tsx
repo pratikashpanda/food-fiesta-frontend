3e44a1aa2a5cc261f2f7a7059de70868d45f8491
pratikash <pratikashpanda.1234@gmail.com>
2024-07-23 15:11:31 +0530

    auth0 complete

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import "./global.css";
+import AppRoutes from "./AppRoutes.tsx";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Router>
+      <Auth0ProviderWithNavigate>
+        <AppRoutes />
+      </Auth0ProviderWithNavigate>
+    </Router>
+  </React.StrictMode>
+);
