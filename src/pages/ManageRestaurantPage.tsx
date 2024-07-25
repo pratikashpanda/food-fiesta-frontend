46270b3682ffd891ba0af5d59300f9dd67ae71f7
pratikash <pratikashpanda.1234@gmail.com>
2024-07-25 19:16:17 +0530

    add restaurant functionality done

---
+++ src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,12 @@
+import { useCreateMyRestaurant, useGetMyRestaurant } from "@/api/MyRestaurantApi";
+import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
+
+const ManageRestaurantPage = () => {
+  const {createRestaurant, isLoading} = useCreateMyRestaurant();
+  const {restaurant} = useGetMyRestaurant()
+  return (
+    <ManageRestaurantForm restaurant={restaurant} onSave={createRestaurant} isLoading={isLoading} />
+  );
+};
+
+export default ManageRestaurantPage;
